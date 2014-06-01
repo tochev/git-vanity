c2b9861a6ee3ce402b8292891ada967d6c719dc9
Tocho Tochev <tocho@tochev.net>
2014-06-01 16:55:05 +0300

    initial public release

---
+++ README.md
@@ -0,0 +1,298 @@
+# git-vanity #
+
+**git-vanity** is a tool for generating git commit hashes with a pre-specified prefix (*vanity commits*).
+
+This is done either by adding a string to the committer's name (note that the visualization utilities usually show the author's name) or by adding custom data to the commit's header.
+
+*Homepage:* https://github.com/tochev/git-vanity
+
+Demo: https://github.com/tochev/git_vanity_demo
+
+
+## Install ##
+
+Install `python3`, `pyopencl`, `numpy`, and `opencl`:
+
+    # substitute amd for nvidia if you have nvidia gpu
+    apt-get install amd-libopencl1 amd-opencl-icd
+    apt-get install python3 python3-numpy python3-pyopencl
+
+Get the project:
+
+    git clone https://github.com/tochev/git-vanity
+
+In order to use the GPU you will need drivers that support OpenCL, which at the moment means that for Radeon you will need the proprietary ones (fglrx).
+
+Optional: create `git-vanity` symlink to `git_vanity.py` in order to be able to call `git vanity`:
+
+    sudo ln -s GIT_VANITY_DIR/git_vanity.py /usr/bin/git-vanity
+
+Optional: export `PYOPENCL_CTX` to avoid being prompted for the OpenCL device to be used (`git-vanity` will prompt you on the first run and display the appropriate information).
+
+
+## Example Usage ##
+
+### Usage ###
+
+    $ git-vanity --help
+    usage: git-vanity [-h] [-s START] [-g GS] [-w WS] [-W] [-q] [-r] [--version]
+                      hex_prefix
+
+    Create vanity commits by extending the committer's name or the commit object.
+
+    positional arguments:
+      hex_prefix            the desired hex prefix
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -s START, --start START
+                            start the search from number (hex)
+      -g GS, --global-size GS
+                            OpenCL global size (use carefully)
+      -w WS, --work-size WS
+                            OpenCL work size (64,128,256,...)
+      -W, --write           write to the repo
+      -q, --quiet           quiet mode, disables progress
+      -r, --raw             change the raw commit instead of the committer
+      --version             show program's version number and exit
+
+
+    $ git deadbeef -W # change HEAD to commit having id starting with deadbeef
+    ...
+
+    $ git log | head -n 1
+    commit deadbeefe959dfe56e86a483a344ca582e0b74c
+
+### Example ###
+
+    $ git log
+    commit 557d8de0090428e170e548131c80fcb0d822545b
+    Author: John Doe <john.doe@example.com>
+    Date:   Sun Apr 13 17:02:19 2014 +0300
+
+        00000000
+
+    commit ffffffff1eee2fa663259abd075621d32d46a16d
+    Author: John Doe <john.doe@example.com>
+    Date:   Sun Apr 13 16:47:57 2014 +0300
+
+        Add file b.txt
+
+        Very interesting commit by John Doe.
+
+    commit ffffffff204994fae3843df6c5be753e88ced44b
+    Author: John Doe <john.doe@example.com>
+    Date:   Sun Apr 13 16:43:56 2014 +0300
+
+        initial commit
+
+    $ git vanity 00000000 -q -W
+    Attempting to find sha1 prefix `00000000'
+    for commit `557d8de0090428e170e548131c80fcb0d822545b'
+    ================
+    tree d11b5fac254c4b7a5a8e078cbad43ba15d6494ff
+    parent ffffffff1eee2fa663259abd075621d32d46a16d
+    author John Doe <john.doe@example.com> 1397397739 +0300
+    committer John Doe <john.doe@example.com> 1397397739 +0300
+
+    00000000
+    ================
+    ...
+
+    Found sha1 prefix `00000000'
+    with sha1 `00000000b2c4731e107f28abfff83f53816b305a'
+    Using 00000000007716D6
+    ================
+    tree d11b5fac254c4b7a5a8e078cbad43ba15d6494ff
+    parent ffffffff1eee2fa663259abd075621d32d46a16d
+    author John Doe <john.doe@example.com> 1397397739 +0300
+    committer John Doe 00000000007716D6 <john.doe@example.com> 1397397739 +0300
+
+    00000000
+    ================
+
+
+    Writing changes to the repository...
+
+    John Doe 00000000007716D6
+    [master 0000000] 00000000
+     Author: John Doe <john.doe@example.com>
+     1 file changed, 1 insertion(+)
+     create mode 100644 c.txt
+    Current HEAD:
+    00000000b2c4731e107f28abfff83f53816b305a
+    All done.
+
+    $ git log
+    commit 00000000b2c4731e107f28abfff83f53816b305a
+    Author: John Doe <john.doe@example.com>
+    Date:   Sun Apr 13 17:02:19 2014 +0300
+
+        00000000
+
+    commit ffffffff1eee2fa663259abd075621d32d46a16d
+    ...
+
+
+## How does it work? ##
+
+**git-vanity** amends the last commit to have a hash that matches a particular prefix.
+
+This is achieved by appending a 64bit number in hexadecimal notation to the committer's name, which is not normally shown. Alternatively, another option offered by git-vanity is to add a vanity field containing a 64bit number to the commit header, which, though unlikely, might interfere with low-level git software or future git versions.
+
+Example: the repository before the change looks like:
+
+    $ git cat-file -p HEAD
+    tree 468948f9e6b55bd3514f554c1c34cbca70a0821f
+    parent 00000000b2c4731e107f28abfff83f53816b305a
+    author John Doe <john.doe@example.com> 1397400447 +0300
+    committer John Doe <john.doe@example.com> 1397400447 +0300
+
+    add random file
+
+After `git vanity deadbeef -W` we get:
+
+    $ git show-ref -s HEAD --head
+    deadbeef0c84b5c33941939582d574c7ddcde9e4
+
+    $ git cat-file -p HEAD
+    tree 468948f9e6b55bd3514f554c1c34cbca70a0821f
+    parent 00000000b2c4731e107f28abfff83f53816b305a
+    author John Doe <john.doe@example.com> 1397400447 +0300
+    committer John Doe 0000000000A92AB3 <john.doe@example.com> 1397400447 +0300
+
+    add random file
+
+
+## Related Work ##
+
+ - bitcoin and litecoin vanity address generators
+ - [vanitygen](https://github.com/samr7/vanitygen) bitcoin address generator
+ - [gitbrute](https://github.com/bradfitz/gitbrute/) CPU vanity git commit generator using committer and author timestamps
+
+## License ##
+
+Distributed under GPL3.
+
+Need another license - I'm flexible - send me an email.
+
+
+## Authors ##
+
+Developed by Tocho Tochev [tocho AT tochev DOT net].
+
+
+## FAQ ##
+
+#### Why OpenCL and not pure CPU? ####
+
+For these types of computations OpenCL is much faster.
+Besides I needed a toy project for playing around with OpenCL.
+
+#### Why the change to the commit is done in this way? ####
+
+If one is to adhere to the human git interface one can change the author's/committer's names and emails and the two timestamps.
+
+There are several reasons to append a fixed-width number to the committer name:
+
+ - the timestamps are preserved
+ - the emails are preserved
+ - no information is lost
+ - it is easy to automatically process it (auto-striping, extracting the original committer, etc.)
+ - it is revertible
+ - successive `git-vanity` applications need not further mangle the commit, they can just use the already allocated space for the number
+ - 64 bits should be sufficient for the computationally feasible search space at the moment, and besides one can always tweak the committer name further
+ - the length of the data during the search does not change, resulting in faster computation
+
+#### What are the consequences of changing the raw commit? ####
+
+At the moment all of the software I'm aware of has not problems dealing with the changes introduced by git vanity. However, one should be aware of the remote possibility of problems with future git versions or low-level git tools.
+
+Use raw commit changes at your own risk.
+
+#### Which commits can be changed? ####
+
+Currently only the HEAD is changed but by manually switching the HEAD one can change all revisions.
+
+#### How fast is it? ####
+
+It depends on your hardware and of course on the length of the commit message, but on a Radeon HD 7750 with a short commit message it does about 70MHash/s, dropping to about 40MHash/s for the long commit messages.
+
+There is a lot of luck involved with finding a matching hash. Therefore, making estimations about the remaining time is prone to error.
+
+When searching prefix of length `X` bits there `2^140 - 2^(140 - X)` hashes that do not match it.
+At each step the chance of finding a matching prefix is assumed to be around `2^X`.
+
+Under the *wrong* assumption that each successive change to the number added to the committer's name gives unique prefix we should find a hash within `2^X` tries (displayed as "tries remaining").
+
+If one assumes that the hash values are uniformly distributed, the probability of `Y` consecutive prefix failures and then success is `1 - (1 - 1/2^X)^Y` (displayed as "Chance (CDF)". This statistic has proven itself valid by the bitcoin community for the sha256 hash.
+
+Generally finding a 32-bit (8-symbol) prefix on Radeon HD 7750 takes a minute-two, while a 40-bit one (10-symbol, as displayed by github) should reach `2^40` hashes tried (`CDF=0.63`) in about 4-8 hours.
+
+#### Why isn't there an estimated time and why does the progress show strange numbers? ####
+
+See the previous question.
+
+#### Does it work with GPG singed commits? ####
+
+Yes, but only in raw commit mode, otherwise the program will not work correctly on a signed commit.
+
+If you apply committer name vanity search on a signed commit you must first remove the signature (you can use `git vanity 0 -W`) and then run it for the desired prefix.
+
+#### Will it always find a hash? ####
+
+The short answer is that if the prefix is short - yes, if it is not - it is up to your luck.
+
+The more elaborate answer is that it tries up to `2^64` changes to the message (which is infeasible on the current home hardware). The chances are that for short prefixes (32 bits) a solution will be found quickly, but for longer ones (48 bits) it might just take too long. Also if you give it insufficient search space by setting start too close to `MAX_UINT64` it will most likely fail to find a solution.
+
+#### Any risks involved? ####
+
+The source code is safe in that it will not damage any data.
+
+Since the program forces the hardware to work hard it needs sufficient cooling. This should not be a problem for cards running with stock settings in well ventilated environment. The rule of the thumb is that if your hardware can do bitcoin/litecoin mining it can handle this vanity search.
+
+That being said, due to potential bugs in the GPU drivers it is possible, although highly unlikely, for the X to crash, for the computer to freeze, etc. A restart will fix the problem and then you can try to tweak down the global and work sizes.
+
+#### It did not work correctly. ####
+
+As with any software bugs happen.
+
+If it seems that the bug is related to code running on the GPU please restart the PC (to rule out any temporally problems in the drivers) and try without any overclocking.
+
+If the problem persists please send me the output of `git cat-file -p HEAD`, your command line, information about your setup (hardware, X, drivers), and any errors.
+I will try to replicate the problem but finding similar setup might be hard.
+
+#### Can I use the CPU? ####
+
+If you have a modern CPU most likely the answer is yes, but it will be very slow compared to the GPU and probably less efficient than a CPU-targeted option.
+
+#### I have a comment. ####
+
+Drop me a mail.
+
+#### Ok, I want to help. What can I do? ####
+
+You can:
+
+ - spread the word
+ - tip me in bitcoin: 1BrWMW6s6Z6EoJobrGZELvnjj8pjS1o5BH or litecoin: LUHDByhZzjaAhnfgnziUpgwuncymRq9qVM
+ - take a look at the list of TODOs and help with something, I accept patches
+ - suggest an improvement
+
+
+## TODOs ##
+
+ - better probability-based counter
+ - better precision in the counters
+ - add quiet mode
+ - use logging
+ - write tests
+ - add more error handling
+ - auto-optimization of GS and WS
+ - add for support other revisions than the HEAD
+ - add more documentation (as usual)
+ - add option for length of the name addition
+ - cl kernel: export the sha1 compute cycle to function, fill W boxes better
+ - rudimentary regex support for the target and optimization
+ - manpage, package, etc
+ - rewrite in C :)
---
+++ git_vanity.py
@@ -0,0 +1,389 @@
+#!/usr/bin/python3
+"""
+git-vanity, a script to make vanity commits
+This is done either by changing the committer's name or the raw commit object.
+Copyright (C) 2014  Tocho Tochev <tocho AT tochev DOT net>
+
+Please tweak GS and WS to suit your video card.
+
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+VERSION = "0.1.0"
+
+import argparse
+import numpy as np
+import os
+import pyopencl as cl
+import re
+import struct
+import subprocess
+import time
+from datetime import timedelta
+from hashlib import sha1
+
+
+GS = 4*1024*1024      # GPU iteration global_size
+WS = 64               # work_size
+MIN_PROGRESS_RESOLUTION = 1.0 # seconds
+
+def hex2target(hex_prefix):
+    """Returns 5*int32 0-padded target and bit length based on hex prefix"""
+    data = hex_prefix + ('0' * (40 - len(hex_prefix)))
+    target = np.array(
+        [int(data[i*8 : (i+1)*8], 16) for i in range(5)],
+        dtype=np.uint32)
+    return target, len(hex_prefix)*4
+
+def progress(start, stop, step, precision_bits, quiet=False):
+    """yields new starts and displays progress"""
+    # deals with uint64
+    assert step > 0
+
+    if not quiet:
+        start_time = time.time()
+        last_progress_time = None
+        last_progress_current = start
+
+    current = start
+    while current < stop:
+
+        if not quiet:
+            time_now = time.time()
+
+            if (not last_progress_time or
+                (time_now - last_progress_time) > MIN_PROGRESS_RESOLUTION):
+                finished = current - start
+                run_time = time_now - start_time
+
+                if not last_progress_time:
+                    print('\b\r\b\r')
+
+                print("Processing GS iteration %s" %
+                    (finished // step + 1))
+                print("   Time:         %s" %
+                        timedelta(seconds=run_time))
+                if last_progress_current == current:
+                    print("   Last Speed:   N/A MH/s      (Avg: N/A MH/s)")
+                else:
+                    print("   Last Speed:   %.4f MH/s     (Avg: %.4f MH/s)" %
+                            (((current - last_progress_current) /
+                                (time_now - last_progress_time)) / 10**6,
+                            (finished / run_time) / 10**6))
+                print("   Tries remaining (optimistic):  %.6f%% ..." %
+                    (100 * (1 - float(finished) / (1 << precision_bits))))
+                print("   Chance (CDF):                  %.6f%% ..." %
+                    (100 * (1 -
+                        (1 - 1.0 / (1 << precision_bits)) ** (finished))))
+
+                last_progress_time = time.time()
+                last_progress_current = current
+
+        yield current
+
+        current += step
+
+def get_padded_size(size):
+    """Returns the size of the text of size `size' after preprocessing"""
+    if (size % 64) > 55:
+        return ((size // 64) + 2) * 64
+    return ((size // 64) + 1) * 64
+
+def sha1_preprocess_data(data):
+    size = get_padded_size(len(data))
+    preprocessed_message = np.zeros(size, dtype=np.ubyte)
+    preprocessed_message[:len(data)] = list(data)
+    preprocessed_message[len(data)] = 0x80
+    preprocessed_message[-8:] = list(struct.pack('>Q', len(data)*8))
+    return preprocessed_message
+
+def load_opencl():
+    """Returns opencl context, queue, program"""
+    CL_PROGRAM = open(
+        os.path.join(
+            os.path.dirname(
+                os.path.realpath(
+                    __file__)),
+            "sha1_prefix_search.cl"),
+        "r").read()
+    ctx = cl.create_some_context()
+    queue = cl.CommandQueue(ctx)
+    prg = cl.Program(ctx, CL_PROGRAM).build()
+    return ctx, queue, prg
+
+def extract_commit(rev):
+    return subprocess.check_output(["git", "cat-file", "-p", rev])
+
+def preprocess_commit_committer_change(commit):
+    """
+    Returns:
+        [commit_with_header_and_placeholder,
+        placeholder_offset,
+        committer_name, committer_mail, committer_date]
+    """
+    commit_lines = list(commit.splitlines())
+
+    committer_index = [i for (i,line) in enumerate(commit_lines)
+                       if line.startswith(b'committer ')][0]
+    committer_line = commit_lines[committer_index]
+
+    match = re.match(br'committer (?P<name>.*?)'
+                     br'(?P<hex> [0-9A-F]{16})? <(?P<mail>.*)> '
+                     br'(?P<date>.*)',
+                     committer_line)
+
+    assert match, "Unable to parse committer line `%s'" % committer_line
+
+    committer_name = match.group('name')
+    committer_mail = match.group('mail')
+    committer_date = match.group('date')
+    # discard match.group('hex'), assume nobody has 64bit hex last name
+
+    prefix = (b'\n'.join(commit_lines[:committer_index]) +
+              b'\ncommitter ' + committer_name + b' ')
+    rest = ((b'F'*16) + b" <" + committer_mail + b"> " + committer_date +
+            b'\n' + b'\n'.join(commit_lines[committer_index + 1:]) + b'\n')
+
+    header = commit_header(len(prefix) + len(rest))
+
+    return (header + prefix + rest,
+            len(header) + len(prefix),
+            committer_name,
+            committer_mail,
+            committer_date)
+
+def preprocess_commit_raw_change(commit):
+    """
+    Returns:
+        [commit_with_header_and_placeholder, placeholder_offset]
+    """
+    commit_lines = list(commit.splitlines())
+    header_end_index = commit_lines.index(b'')
+
+    vanity_token = b'vanity'
+    if commit_lines[header_end_index - 1] == b' -----END PGP SIGNATURE-----':
+        insert_index = header_end_index - 1
+        vanity_token = b' vanity'
+    else:
+        insert_index = header_end_index
+        vanity_token = b'vanity '
+
+    if commit_lines[insert_index - 1].startswith(vanity_token):
+        insert_index = insert_index - 1
+        commit_lines.pop(insert_index)
+
+    prefix = b'\n'.join(commit_lines[:insert_index]) + b'\n' + vanity_token
+    rest = ((b'F'*16) + b'\n' +
+             b'\n'.join(commit_lines[insert_index:]) + b'\n')
+
+    header = commit_header(len(prefix) + len(rest))
+
+    return (header + prefix + rest, len(header) + len(prefix))
+
+def commit_header(commit_len):
+    return bytes('commit %d\x00' % commit_len, 'ascii')
+
+def commit_add_header(commit):
+    return commit_header(len(commit)) + commit
+
+def commit_without_header(commit):
+    null_index = commit.find(b'\x00')
+    if null_index == -1:
+        return commit
+    return commit[null_index + 1:]
+
+def sha1_prefix_search_opencl(data, hex_prefix, offset,
+                              start=0, stop=(1 << 64),
+                              opencl_vars=None,
+                              gs=GS, ws=WS,
+                              quiet=False):
+    """Return %016x.upper() or raises a ValueError if nothing is found"""
+    if opencl_vars is None:
+        opencl_vars = load_opencl()
+        ctx, queue, prg = opencl_vars
+
+    assert gs % ws == 0, "Global size must be a multiple of work size"
+
+    target, precision_bits = hex2target(hex_prefix)
+    preprocessed_message = sha1_preprocess_data(data)
+
+    result = np.zeros(2, dtype=np.uint64)
+
+    mf = cl.mem_flags
+    # create buffers
+    message_buf = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR,
+                            hostbuf=preprocessed_message)
+    target_buf = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR,
+                           hostbuf=target)
+    result_buf = cl.Buffer(ctx, mf.WRITE_ONLY | mf.COPY_HOST_PTR,
+                           hostbuf=result)
+
+    # the main stuff
+    for current_start in progress(start, stop, gs, precision_bits, quiet):
+        prg.sha1_prefix_search(queue,
+                               (gs,),
+                               (ws,),
+                               message_buf,
+                               struct.pack('I', preprocessed_message.shape[0]),
+                               target_buf,
+                               struct.pack('I', precision_bits),
+                               struct.pack('I', offset),
+                               struct.pack('Q', current_start),
+                               result_buf)
+
+        cl.enqueue_copy(queue, result, result_buf)
+
+        if result[0]: # we found it
+            return ('%016x' % result[1]).upper()
+
+    else:
+        raise ValueError("Unable to find matching prefix...")
+
+def amend_commit_using_committer(committer_name,
+                                 committer_mail,
+                                 committer_date,
+                                 hex_magic):
+    env = os.environ.copy()
+    env['GIT_COMMITTER_NAME'] = committer_name.decode() + " " + hex_magic
+    env['GIT_COMMITTER_EMAIL'] = committer_mail.decode()
+    env['GIT_COMMITTER_DATE'] = committer_date.decode()
+
+    print(env['GIT_COMMITTER_NAME'])
+
+    subprocess.check_call(['git', 'commit', '--amend', '--no-edit',
+                           '-c', 'HEAD'], env=env)
+    print('Current HEAD:')
+    subprocess.check_call(['git', 'rev-parse', 'HEAD'])
+
+def amend_commit_using_raw(object_contents):
+    cmd = subprocess.Popen(
+                ['git', 'hash-object', '-w', '-t', 'commit', '--stdin'],
+                stdin=subprocess.PIPE, stdout=subprocess.PIPE
+            )
+    commit = cmd.communicate(input=object_contents)[0].strip()
+
+    subprocess.check_call(['git', 'update-ref', 'HEAD', commit])
+    print('Current HEAD:')
+    subprocess.check_call(['git', 'rev-parse', 'HEAD'])
+
+
+def main(hex_prefix, start=0, gs=GS, ws=WS, write_changes=False, quiet=False,
+         use_raw_changes=False):
+    """
+    Attempts to change in the current directory
+    hex_prefix is the desired prefix
+    start is int or hex string
+    """
+    commit = extract_commit('HEAD')
+
+    if use_raw_changes:
+        [data, placeholder_offset] = preprocess_commit_raw_change(commit)
+    else:
+        [data,
+         placeholder_offset,
+         committer_name,
+         committer_mail,
+         committer_date] = preprocess_commit_committer_change(commit)
+
+    if isinstance(start, str):
+        start = int(start, 16)
+
+    print(("Attempting to find sha1 prefix `%s'\n"
+           "for commit `%s'\n"
+           "================\n%s================\n\n")
+          % (hex_prefix,
+             sha1(commit_add_header(commit)).hexdigest(),
+             commit.decode()))
+
+    result = sha1_prefix_search_opencl(data,
+                                       hex_prefix,
+                                       placeholder_offset,
+                                       start,
+                                       gs=gs, ws=ws,
+                                       quiet=quiet)
+
+    final = (data[:placeholder_offset] +
+             result.encode() +
+             data[placeholder_offset + 16:])
+    final_object = commit_without_header(final)
+
+    print(("\nFound sha1 prefix `%s'\n"
+           "with sha1 `%s'\n"
+           "Using %s\n"
+           "================\n%s================\n\n")
+          % (hex_prefix,
+             sha1(final).hexdigest(),
+             result,
+             final_object.decode()))
+
+    if write_changes:
+        print("Writing changes to the repository...\n")
+        if use_raw_changes:
+            amend_commit_using_raw(final_object)
+        else:
+            amend_commit_using_committer(committer_name,
+                                         committer_mail,
+                                         committer_date,
+                                         result)
+        print("All done.")
+    else:
+        print("Changes not written to the repository.")
+
+
+if __name__ ==  '__main__':
+    parser = argparse.ArgumentParser(
+        prog="git-vanity",
+        description="Create vanity commits "
+                    "by extending the committer's name or the commit object.")
+    parser.add_argument('hex_prefix',
+                        type=str,
+                        help="the desired hex prefix")
+    parser.add_argument('-s', '--start',
+                        default='0',
+                        type=lambda x: int(x, 16),
+                        help="start the search from number (hex)")
+    parser.add_argument('-g', '--global-size',
+                        dest='gs',
+                        default=GS,
+                        type=int,
+                        help="OpenCL global size (use carefully)")
+    parser.add_argument('-w', '--work-size',
+                        dest='ws',
+                        default=WS,
+                        type=int,
+                        help="OpenCL work size (64,128,256,...)")
+    parser.add_argument('-W', '--write',
+                        action='store_true',
+                        default=False,
+                        help="write to the repo")
+    parser.add_argument('-q', '--quiet',
+                        action='store_true',
+                        default=False,
+                        help="quiet mode, disables progress")
+    parser.add_argument('-r', '--raw',
+                        action='store_true',
+                        default=False,
+                        help="change the raw commit instead of the committer")
+    parser.add_argument('--version',
+                        action='version',
+                        version="%(prog)s " + VERSION)
+
+    args = parser.parse_args()
+
+    main(args.hex_prefix, args.start,
+         args.gs, args.ws,
+         args.write,
+         args.quiet,
+         args.raw)
